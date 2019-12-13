e2a6bbe7afd60994593a0e966fc3fe0abfc9ad5d
irtengunica <irtengunica@gmail.com>
2019-12-13 16:45:41 +0300

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,20 @@
+// ConsoleApplication3.cpp : Defines the entry point for the console application.
+//
+#using <System.dll>;
+#include "stdafx.h"
+#include "iostream" 
+using namespace std;  
+using namespace System;
+using namespace System::IO::Ports;
+using namespace System::Threading;
+
+
+
+int main()
+{
+
+	cout << "Hello\n";
+
+	return 0;
+}
+
