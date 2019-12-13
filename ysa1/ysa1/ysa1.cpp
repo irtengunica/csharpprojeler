262220db4f1f2bcfba3226ccaa76fab7a381f8a8
irtengunica <irtengunica@gmail.com>
2019-12-13 17:45:49 +0300

    Add files via upload

---
+++ ysa1/ysa1/ysa1.cpp
@@ -0,0 +1,13 @@
+// ysa1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	float[] inputs = { 12, 4 };
+	float[] weights = { 0.5, -1 };
+	return 0;
+}
+
---
+++ ysa2/ysa2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ysa2
+{
+    class Program
+    {
+
+      
+        static void Main(string[] args)
+        {
+            float[] inputs = { 12, 4 };
+            float[] weights = { 5, -1 };
+           
+double sum = 0;
+            int output1=0;
+for (int i = 0; i < 2; i++) 
+{
+  sum += inputs[i]*weights[i];
+    if (sum > 0)
+    {
+        output1 = 1;
+    }
+    else
+    {
+        output1 = -1;
+    }
+}
+
+
+        }
+
+    }
+}
