01176915460794b96e58c2428710e152ab632dce
irtengunica <irtengunica@gmail.com>
2019-12-13 16:59:17 +0300

    Add files via upload

---
+++ ConsoleApplication10/ConsoleApplication10/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static SerialPort SeriPortum;
+        static void Main(string[] args)
+        {
+            string argutopla="";
+            SeriPortum = new SerialPort();
+            SeriPortum.PortName = "COM11";
+            SeriPortum.Parity = Parity.None;
+            SeriPortum.BaudRate = 9600;
+            SeriPortum.DataBits = 8;
+            SeriPortum.StopBits = StopBits.One;
+            try
+            {
+                if (SeriPortum.IsOpen == false)  //Seri port açık değilse seri port açılıyor
+                {
+                    SeriPortum.Open();           //Seri porttan veri iletişimi böylece başlamış oluyor   
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Bağlantı kurulamadı! Lütfen girdiğiniz port değerlerini kontrol edin:" + SeriPortum.PortName.ToString() + "," + SeriPortum.BaudRate.ToString());
+                return;
+            }
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Arg: {0}", args[i]);
+                argutopla += " "+args[i]  ;
+            }
+            Console.WriteLine(argutopla);
+            //SeriPortum.WriteLine(" LR1800 UD1800 LZ1");
+           // argutopla = " LR1700 UD1700 LZ0";
+            SeriPortum.WriteLine(argutopla);
+            /*if (args == null)
+            {
+                SeriPortum.WriteLine("LR2000 UD1000 LZ1"); 
+            }
+            else
+            {
+                SeriPortum.WriteLine(args[1]);
+            }*/
+            //SeriPortum.Close();
+        }
+    }
+}
---
+++ ConsoleApplication11/ConsoleApplication11/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO.Ports;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static SerialPort SeriPortum;
+        private static string argutopla;
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(new ThreadStart(ThreadFuncEven));
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Arg: {0}", args[i]);
+                argutopla += " " + args[i];
+            }
+           // argutopla = " LR1000 UD800 LZ1";
+            thread1.Start();
+        }
+        static void ThreadFuncEven()
+        {
+            SeriPortum = new SerialPort();
+            SeriPortum.PortName = "COM11";
+            SeriPortum.Parity = Parity.None;
+            SeriPortum.BaudRate = 9600;
+            SeriPortum.DataBits = 8;
+            SeriPortum.StopBits = StopBits.One;
+            try
+            {
+                if (SeriPortum.IsOpen == false)  //Seri port açık değilse seri port açılıyor
+                {
+                    SeriPortum.Open();           //Seri porttan veri iletişimi böylece başlamış oluyor   
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Bağlantı kurulamadı! Lütfen girdiğiniz port değerlerini kontrol edin:" + SeriPortum.PortName.ToString() + "," + SeriPortum.BaudRate.ToString());
+                return;
+            }
+            SeriPortum.WriteLine(argutopla);
+            Thread.Sleep(1000);
+            SeriPortum.Close();
+            
+        }
+    }
+}
