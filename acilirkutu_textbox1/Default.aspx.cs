757cff405a7bd5b3db26668001ece530b4964155
irtengunica <irtengunica@gmail.com>
2019-12-14 10:51:08 +0300

    Add files via upload

---
+++ acilirkutu_textbox1/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.OleDb;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" +
+                                                                Server.MapPath("App_Data/veritaban.mdb"));
+        OleDbCommand tabloyabaglan = new OleDbCommand("select * from kulanici", baglanti);
+        baglanti.Open();
+        OleDbDataReader tablodanverial = tabloyabaglan.ExecuteReader();
+        GridView1.DataSource = tablodanverial;
+        GridView1.DataBind();
+        baglanti.Close();
+    }
+}
