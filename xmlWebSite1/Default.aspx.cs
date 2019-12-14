560d12c78ad80422fded1bcd06862673a24307ce
irtengunica <irtengunica@gmail.com>
2019-12-14 10:58:30 +0300

    Add files via upload

---
+++ xmlWebSite1/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.IO;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (!File.Exists(Server.MapPath("xml/KullaniciBilgirapor.xml")))
+        {
+            try
+            {
+                XmlTextWriter xmlolustur = new XmlTextWriter(Server.MapPath("xml/KullaniciBilgirapor.xml"), null);//ilk parametre dosyanın oluşturulacağı lokasyon, ikinci parametre encoding
+
+                xmlolustur.WriteStartDocument();//xml içinde element oluşturma işlemine başlandı
+
+                xmlolustur.WriteComment("Kullanıcı Bilgileri Detayları Raporu");//dosya içine bir açıklama satırı eklendi
+
+                xmlolustur.WriteStartElement("Rapor");//bir etiket oluşturduk
+
+                xmlolustur.WriteEndDocument();//element oluşturma işlemi bitti
+                Label1.Text = "Dosya Oluşturuldu";
+                xmlolustur.Close();//dosya oluşturuldu ve işlemler tamamlandı
+            }
+            catch
+            {
+                Label1.Text = "Dosya Hata oldu. Lütfen Tekrar Deneyin";
+            }
+        }
+    }
+}
---
+++ xmlWebSite1/okuma.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.IO;
+
+public partial class okuma : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load("http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml");
+        foreach (XmlNode xmlNode in xmlDoc.DocumentElement.ChildNodes[2].ChildNodes[0].ChildNodes)
+            Response.Write(xmlNode.Attributes["currency"].Value + ": " + xmlNode.Attributes["rate"].Value);
+        
+    }
+}
